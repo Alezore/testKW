ebd9f0e81d4037656d803c0af1d07968f3479b36
Alezore <79982180+Alezore@users.noreply.github.com>
2025-03-19 15:29:13 +0100

    kgyhf
    poimlikjh,gnf

--- Testing/Templates/.#scripts/Testing.template.js
+++ Testing/Templates/.#scripts/Testing.template.js
@@ -1,11 +1,11 @@
-//__Script: //Testing/Templates/Testing.template; Data4@Span0@Par0@Container0@Section0@Document0@RootPart@.library.Testing.Templates.Testing.template@.script
+//__Script: //Testing/Templates/Testing.template; Data2@Span0@Par0@Container0@Section0@Document0@RootPart@.library.Testing.Templates.Testing.template@.script
 $data.data1
 
-//__Script: //Testing/Templates/Testing.template; Data4@Span0@Par39@Container0@Section0@Document0@RootPart@.library.Testing.Templates.Testing.template@.script
+//__Script: //Testing/Templates/Testing.template; Data2@Span0@Par39@Container0@Section0@Document0@RootPart@.library.Testing.Templates.Testing.template@.script
 $data.data2
 
-//__Script: //Testing/Templates/Testing.template; Data4@Span0@Par0@StaticContainer1@Section0@Document0@RootPart@.library.Testing.Templates.Testing.template@.script
+//__Script: //Testing/Templates/Testing.template; Data2@Span0@Par0@StaticContainer1@Section0@Document0@RootPart@.library.Testing.Templates.Testing.template@.script
 $data.zone1
 
-//__Script: //Testing/Templates/Testing.template; Data4@Span0@Par0@StaticContainer2@Section0@Document0@RootPart@.library.Testing.Templates.Testing.template@.script
+//__Script: //Testing/Templates/Testing.template; Data2@Span0@Par0@StaticContainer2@Section0@Document0@RootPart@.library.Testing.Templates.Testing.template@.script
 $data.zone2
