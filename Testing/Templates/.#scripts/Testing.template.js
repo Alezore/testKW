994f2c1dfd35ab84f37ce4791ec3bbbebd243cb9
Alezore <79982180+Alezore@users.noreply.github.com>
2025-04-08 10:44:39 +0200

    added tonic editior highligh

--- Testing/Templates/.#scripts/Testing.template.js
+++ Testing/Templates/.#scripts/Testing.template.js
@@ -4,7 +4,7 @@ $data.data1
 //__Script: //Testing/Templates/Testing.template; Data2@Span0@Par39@Container0@Section0@Document0@RootPart@.library.Testing.Templates.Testing.template@.script
 $data.data2
 
-//__Script: //Testing/Templates/Testing.template; Data2@Span0@Par0@StaticContainer1@Section0@Document0@RootPart@.library.Testing.Templates.Testing.template@.script
+//__Script: //Testing/Templates/Testing.template; Data2@Span0@Par0@StaticContainer@zone1@.script
 $data.zone1
 
 //__Script: //Testing/Templates/Testing.template; Data2@Span0@Par0@StaticContainer2@Section0@Document0@RootPart@.library.Testing.Templates.Testing.template@.script
